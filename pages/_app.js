e95c5b3509a80a50f298a7eba1b593cfa19fb600
Malte I.Lauterbach <malte@enigma-blog.de>
2020-12-30 16:34:02 +0100

    undid error

--- pages/_app.js
+++ pages/_app.js
@@ -20,6 +20,47 @@ function MyApp({ Component, pageProps }) {
   console.log(pageProps);
   return (
     <ChakraProvider theme={theme}>
+      <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+          <Box position="fixed" bottom="90px" width="100%">
+            <Navigation
+              onSelect={({ itemId }) => {
+                router.push(itemId);
+              }}
+              items={[
+                {
+                  title: "Home",
+                  itemId: "/",
+                  // you can use your own custom Icon component as well
+                  // icon is optional
+                },
+                {
+                  title: "About us",
+                  itemId: "/about",
+                },
+              ]}
+            />
+        </Box>
+        <Box
+          width={["100vw", "calc(95vw)", "calc(95vw)"]}
+          paddingLeft={["10px", "250px", "250px"]}
+          paddingRight={["10px", "0px", "0px"]}
+          paddingBottom={["10px", "10px"]}
+          paddingTop={["30px", "70px", "70px"]}
+        >
+          <Link href="/">
+            <Heading pb="3" display={["block", "none", "none"]} textAlign="center">
+              Custos Vera
+            </Heading>
+          </Link>
+          <Link href="/about">
+              <Text pb="4" display={["block", "none", "none"]} textAlign="center" fontWeight="bold">
+                About us
+              </Text>
+          </Link>
+          <Component {...pageProps} />
+          <hr style={{ marginTop: "20px", paddingBottom: "10px" }} />
+	        </Box>
+      </Grid>
     </ChakraProvider>
   );
 }
